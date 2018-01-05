a2d810b90f6648ac81db6cd124b6c26b13394ef7
tc-imba <liuyh615@sjtu.edu.cn>
2018-01-05 21:29:29 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const _package = require("./package.json");
+const MAX_OPENING = _package.openings;
+
+let openings = [];
+for (let i = 1; i <= MAX_OPENING; i++) {
+    openings.push(require(`openings/opening${i}.json`));
+}
+
+function check(num) {
+    num = parseInt(num);
+    if (num < 1 || num > MAX_OPENING) {
+        throw new Error('opening num out of range');
+    }
+    return num;
+}
+
+module.exports = {
+    get: function (num) {
+        num = check(num);
+        return openings[num - 1];
+    },
+    random: function (num) {
+        num = check(num);
+        let data = [];
+        for (let i = 0; i < MAX_OPENING; i++) {
+            data.push(i);
+        }
+        for (let i = 0; i < MAX_OPENING; i++) {
+            let j = Math.floor(Math.random() * MAX_OPENING);
+            let temp = data[i];
+            data[i] = data[j];
+            data[j] = temp;
+        }
+        let result = [];
+        for (let i = 0; i < num; i++) {
+            result.push(openings[data[i]]);
+        }
+    }
+};
